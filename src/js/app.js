e70f0a4833b7db6662a8d8a8e382205bb54711fa
odinokun <odinokun@gmail.com>
2020-06-03 21:02:27 +0300

    did about page

--- src/js/app.js
+++ src/js/app.js
@@ -13,13 +13,10 @@ $(function() {
   // require('./modules/scroll2ancor')();
 
   // begin Slick slider
-  // require('./modules/slick')();
+  require('./modules/slick')();
 
   // begin Popup
   // require('./modules/popup')();
-
-  // begin Sticky header
-  // require('./modules/stickyHeader')();
 
   // begin Animate.css
   //РАБОТАЕТ ТОЛЬКО С JQUERY < 3.0.0 !!!!!!!!!!!!!!!!!!
--- src/js/modules/slick.js
+++ src/js/modules/slick.js
@@ -1,16 +1,23 @@
 module.exports = function() {
 
   // begin Slick slider
-
-  $('.slick').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    autoplay: true,
-    cssEase: 'linear'
+  $('#about-gallery__list').slick({
+    dots: false,
+    infinite: false,
+    autoplay: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: "<div class='prev'><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"33\" height=\"14\" viewBox=\"0 0 33 14\"><g><g><path d=\"M6.173.33L.55 5.952c-.44.439-.44 1.15 0 1.59l5.623 5.623a1.125 1.125 0 0 0 1.59-1.59L4.06 7.872h27.036a1.125 1.125 0 0 0 0-2.249H4.06L7.763 1.92a1.121 1.121 0 0 0 0-1.59 1.125 1.125 0 0 0-1.59 0z\"/></g></g></svg></div>",
+    nextArrow: "<div class='next'><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"33\" height=\"14\" viewBox=\"0 0 33 14\"><g><g><path d=\"M26.33 13.166l5.623-5.623c.439-.44.439-1.151 0-1.59L26.33.329a1.125 1.125 0 0 0-1.59 1.59l3.702 3.704H1.407a1.125 1.125 0 0 0 0 2.25h27.035l-3.703 3.703a1.121 1.121 0 0 0 0 1.59c.44.44 1.151.44 1.59 0z\"/></g></g></svg></div>",
+    responsive: [
+      {
+        breakpoint: 481,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
   });
-
   // end Slick slider
 
 };
